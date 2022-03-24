c5815850c735bc946f1e28673ddc128c22705438
ChipsCmoa <92235023+ChipsCmoa@users.noreply.github.com>
2022-03-24 11:31:25 +0100

    Update chat.js

--- src/events/chat.js
+++ src/events/chat.js
@@ -1,19 +1,6 @@
-const fs = require('fs');
-
 module.exports = {
 	name: 'chat',
 	execute(client, channel, params, txt, self) {
-		/*
-		//////////// DEBUT LOGS-CHAT
-		let timestamp;
-		if (self) timestamp = String(new Date().getTime());
-		if (!self) timestamp = params["tmi-sent-ts"];
-		const content = timestamp.slice(0, -3) + "," + channel.slice(1) + "," + params["display-name"] + "," + txt + "\n";
-		fs.appendFile('././logs/logs-chat.csv', content, function (err) {
-			if (err) throw err;
-		});
-		//////////// FIN LOGS-CHAT
-		*/
 		if (self) return;
 		if (params.username === "nightbot" && txt === "Oh baggy... T'es dégueu !") client.say(channel, `Ce n'était pas moiiiii frzodBaggycry1`);
 
@@ -29,9 +16,3 @@ module.exports = {
 		}
 	}
 };
-/*
-console.log("//////////////////////NOUVEAU MESSAGE//////////////////////");
-console.log(params);
-console.log(txt);
-console.log("");
-*/
